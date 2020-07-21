d5a2658adec2f5d4aadb84cf74529df9f8de09a6
Sze Kim Leong <szekim.leong@siemens.com>
2020-07-21 23:33:31 +0800

    update our menu-tems to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <duv>
+    <h1>HATS PAGE</h1>
+  </duv>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </switch>
     </div>
   );
 }
