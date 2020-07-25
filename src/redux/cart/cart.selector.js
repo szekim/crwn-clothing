a18090fd7376fa4c396bcede3f1dd46b23345c8b
Sze Kim Leong <szekim.leong@siemens.com>
2020-07-25 23:19:02 +0800

    added checkout page, checkout item component and updated cart to use selector from reselect

--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -7,6 +7,21 @@ export const selectCartItems = createSelector(
   (cart) => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+)
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
+        0
+    )
+)
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) =>
